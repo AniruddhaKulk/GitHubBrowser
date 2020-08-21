b1049d2182525f80c7779bd17b09f9a66ce5b48f
AniruddhaKulk <anikulki@gmail.com>
2020-08-21 12:33:41 +0530

    Add home module and create a HomeFragment.

---
+++ home/src/main/java/com/anikulki/daggergithub/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.anikulki.daggergithub.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.anikulki.daggergithub.home.databinding.HomeFragmentBinding
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
