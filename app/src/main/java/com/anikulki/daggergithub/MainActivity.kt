b0444de1ec8d2ee3a87c9ef2fdfe05f809242502
AniruddhaKulk <anikulki@gmail.com>
2020-08-23 17:26:54 +0530

    Setup recyclerview in HomeFragment and show dummy data

--- app/src/main/java/com/anikulki/daggergithub/MainActivity.kt
+++ app/src/main/java/com/anikulki/daggergithub/MainActivity.kt
@@ -2,10 +2,17 @@ package com.anikulki.daggergithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anikulki.daggergithub.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, HomeFragment())
+                .commit()
+        }
     }
 }
--- home/src/main/java/com/anikulki/daggergithub/home/HomeFragment.kt
+++ home/src/main/java/com/anikulki/daggergithub/home/HomeFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.anikulki.daggergithub.di.viewmodel.AppViewModelFactory
 import com.anikulki.daggergithub.home.databinding.HomeFragmentBinding
+import com.anikulki.daggergithub.home.list.HomeRepoAdapter
 import javax.inject.Inject
 
 class HomeFragment: Fragment() {
@@ -31,6 +35,51 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater, container, false)
+
+        binding.rvRepoList.apply {
+            adapter = HomeRepoAdapter()
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+        }
+
+        homeViewModel.viewStateUpdate.observe(viewLifecycleOwner, Observer{state ->
+
+            when(state){
+                is HomeViewStateLoading -> handleLoadingState(binding)
+                is HomeViewStateLoaded -> handleLoadedState(state, binding)
+                is HomeViewStateError -> handleErrorState(state, binding)
+            }
+
+        })
+
+
         return binding.root
+    }
+
+    private fun handleLoadedState(state: HomeViewStateLoaded, binding: HomeFragmentBinding) {
+        binding.rvRepoList.visibility = View.VISIBLE
+        binding.tvError.visibility = View.GONE
+        binding.progressBar.visibility = View.GONE
+
+        (binding.rvRepoList.adapter as HomeRepoAdapter).setRepoList(state.list)
+    }
+
+    private fun handleErrorState(state: HomeViewStateError, binding: HomeFragmentBinding) {
+        binding.rvRepoList.visibility = View.GONE
+        binding.tvError.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.GONE
+
+        binding.tvError.text = state.message
+    }
+
+    private fun handleLoadingState(binding: HomeFragmentBinding) {
+        binding.rvRepoList.visibility = View.GONE
+        binding.tvError.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
     }
 }
